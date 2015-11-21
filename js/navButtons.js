0767d4ad456a4a69e383ca868f2dbe73ad16e59f
Mauricio <mau.mruiz@hotmail.com>
2015-11-21 16:51:40 -0600

    Home ajax calls

--- js/navButtons.js
+++ js/navButtons.js
@@ -9,14 +9,14 @@ $(document).on('ready',function() {
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         success: function(jsonData) {
             var navButtons = '<li class="dropdown">' +
-                '<a href="#" class="dropdown-toggle" data-toggle="dropdown"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span> User <b class="caret"></b></a>' +
+                '<a href="#" class="dropdown-toggle" data-toggle="dropdown"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span> ' + jsonData.firstName + ' ' + jsonData.lastName + '<b class="caret"></b></a>' +
                 '<ul class="dropdown-menu">'+
                     '<li><a href="userProfile.html">My Profile</a></li>' +
                     '<li><a href="orderHistory.html">Order History</a></li>' +
                     '<li class="divider"></li>' +
                     '<li><a href="uploadDesign.html">Upload Design</a></li>' +
                     '<li class="divider"></li>' +
-                    '<li><a href="#">Logout</a></li></ul></li>' +
+                    '<li><a id="logoutButton">Logout</a></li></ul></li>' +
                 '<li><a href="cart.html"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Cart </a></li>';
 
                 $("#navRightButtons").html(navButtons);
@@ -28,4 +28,22 @@ $(document).on('ready',function() {
         }
     });
 
+});
+
+// Setting the action for the logout tab
+$(document).on('click','#logoutButton',function(){
+    $.ajax({
+        type: 'POST',
+        url: 'data/applicationLayer.php',
+        dataType: 'json',
+        data: {'action':'END_SES'},
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        success: function(jsonData) {
+            window.location.reload();
+        },
+        error: function(errorMsg){
+            alert(errorMsg.statusText);
+            window.location.reload();
+        }
+    });
 });
